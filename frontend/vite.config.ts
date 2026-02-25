503c14ca9345d877f6700206355a315ee516edb7
PranavTJ-05 <pranav.tj150605@gmail.com>
2026-02-25 18:45:56 +0530

    strictport changed to false

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    strictPort: true,
+    strictPort: false,
     allowedHosts: ['edu-resource-frontend.onrender.com'],
   },
   build: {
